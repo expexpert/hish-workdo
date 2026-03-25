cee65f76a692ba4c30d4ace689ada5b861747d9e
Web Test <upworkwebexpert@gmail.com>
2026-03-25 19:13:25 +0530

    worked on adding filter functionality

--- resources/views/ClientReport/invoice.blade.php
+++ resources/views/ClientReport/invoice.blade.php
@@ -112,6 +112,55 @@
 </div>
 
 <div class="row">
+    <div class="col-sm-12">
+        <div class=" multi-collapse mt-2 " id="multiCollapseExample1">
+            <div class="card">
+                <div class="card-body">
+                    {{ Form::open(['route' => ['customer.invoices'], 'method' => 'GET', 'id' => 'customer_submit']) }}
+                    <div class="row d-flex align-items-center justify-content-end">
+                        <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12 mr-2">
+                            <div class="btn-box">
+                                {{ Form::label('start_date', __('Start Date'), ['class' => 'text-type']) }}
+                                {{ Form::date('start_date', isset($_GET['start_date']) ? $_GET['start_date'] : '', ['class' => 'form-control', 'placeholder' => __('YYYY-MM-DD')]) }}
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12 mr-2">
+                            <div class="btn-box">
+                                {{ Form::label('end_date', __('End Date'), ['class' => 'text-type']) }}
+                                {{ Form::date('end_date', isset($_GET['end_date']) ? $_GET['end_date'] : '', ['class' => 'form-control', 'placeholder' => __('YYYY-MM-DD')]) }}
+                            </div>
+                        </div>
+                        @if (!\Auth::guard('customer')->check())
+                        <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12 mr-2">
+                            <div class="btn-box">
+                                {{ Form::label('customer', __('Customer'), ['class' => 'text-type']) }}
+                                {{ Form::select('customer', $customer, isset($_GET['customer']) ? $_GET['customer'] : '', ['class' => 'form-control']) }}
+                            </div>
+                        </div>
+                        @endif
+                        <div class="col-auto d-flex mt-4">
+
+                            <a href="#" class="btn btn-sm btn-primary me-2"
+                                onclick="document.getElementById('customer_submit').submit(); return false;"
+                                data-bs-toggle="tooltip" title="{{ __('Search') }}"
+                                data-original-title="{{ __('Apply') }}">
+                                <span class="btn-inner--icon"><i class="ti ti-search"></i></span>
+                            </a>
+                            <a href="{{ route('customer.invoices') }}" class="btn btn-sm btn-danger"
+                                data-bs-toggle="tooltip" title="{{ __('Reset') }}">
+                                <span class="btn-inner--icon"><i class="ti ti-refresh text-white-off"></i></span>
+                            </a>
+                        </div>
+
+                    </div>
+                    {{ Form::close() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
     <div class="col-md-12">
         <div class="card">
             <div class="card-body table-border-style">
@@ -164,11 +213,11 @@
                                 @php
                                 $totalTtc = 0;
                                 if ($invoice->articles && $invoice->articles->count()) {
-                                 $totalTtc=$invoice->articles->sum(function ($article) {
-                                    $ht = floatval($article->total_price_ht ?? 0);
-                                    $tvaPct = floatval($article->tva_percentage ?? 0) / 100;
-                                    return $ht + ($ht * $tvaPct);
-                                 });
+                                $totalTtc=$invoice->articles->sum(function ($article) {
+                                $ht = floatval($article->total_price_ht ?? 0);
+                                $tvaPct = floatval($article->tva_percentage ?? 0) / 100;
+                                return $ht + ($ht * $tvaPct);
+                                });
                                 }
                                 @endphp
                                 <td>{{ \Auth::user()->priceFormat($totalTtc) }}</td>
@@ -357,7 +406,6 @@
                         };
                         e.target.className = `form-select form-select-sm fw-bold border-2 transition w-100 invoice-action ${styleMap[action]}`;
 
-                        console.log(data.counts);
                         // Update counts
                         document.getElementById('total-invoices').textContent = data.counts.total;
                         document.getElementById('pending-invoices').textContent = data.counts.pending;
