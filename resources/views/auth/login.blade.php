5681894ad730a67c15b9fafc25f7af98eba4113f
Web Test <upworkwebexpert@gmail.com>
2026-03-25 15:06:56 +0530

    worked on landing page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,147 +1,194 @@
 @extends('layouts.auth')
 @php
-    use App\Models\Utility;
-    $logo = asset(Storage::url('uploads/logo/'));
-    $settings = Utility::settings();
+use App\Models\Utility;
+$logo = asset(Storage::url('uploads/logo/'));
+$settings = Utility::settings();
 @endphp
 @section('page-title')
-    {{ __('Login') }}
+{{ __('Login') }}
 @endsection
 
 @section('auth-lang')
-    @php
-        $languages = App\Models\Utility::languages();
-    @endphp
-    <div class="lang-dropdown-only-desk">
-        <li class="dropdown dash-h-item drp-language">
-            <a class="dash-head-link dropdown-toggle btn" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-                <span class="drp-text"> {{ ucfirst($languages[$lang] ?? '') }}
-                </span>
+@php
+$languages = App\Models\Utility::languages();
+@endphp
+<div class="lang-dropdown-only-desk">
+    <li class="dropdown dash-h-item drp-language">
+        <a class="dash-head-link dropdown-toggle btn" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+            <span class="drp-text"> {{ ucfirst($languages[$lang] ?? '') }}
+            </span>
+        </a>
+        <div class="dropdown-menu dash-h-dropdown dropdown-menu-end">
+            @foreach ($languages as $code => $language)
+            <a href="{{ route('login', $code) }}" tabindex="0"
+                class="dropdown-item {{ $code == $lang ? 'active' : '' }}">
+                <span>{{ ucFirst($language) }}</span>
             </a>
-            <div class="dropdown-menu dash-h-dropdown dropdown-menu-end">
-                @foreach ($languages as $code => $language)
-                    <a href="{{ route('login', $code) }}" tabindex="0"
-                        class="dropdown-item {{ $code == $lang ? 'active' : '' }}">
-                        <span>{{ ucFirst($language) }}</span>
-                    </a>
-                @endforeach
-            </div>
-        </li>
-    </div>
+            @endforeach
+        </div>
+    </li>
+</div>
 @endsection
 
+<head>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="{{ asset('storage/uploads/new-landing-page/header_logo.png') }}">
+    <link rel="stylesheet" href="{{ asset('/css/landingPage.css') }}" />
+</head>
+
+<style>
+    header.dash-header {
+        display: none;
+    }
+
+    body.theme-3 .bg-primary {
+        background: white !important;
+    }
+</style>
+<div class="sc-dash">
+    <!-- Header Section -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('storage/uploads/new-landing-page/header_logo.svg')}}" alt="Simply Compta Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Fonctionnalités</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Mission</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-success" href="/login">Essai gratuit</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</div>
+
 @section('content')
-    <div class="d-flex align-items-center justify-content-between">
-        <h2 class="mb-3 f-w-600">{{ __('Login') }}</h2>
+<div class="d-flex align-items-center justify-content-between">
+    <h2 class="mb-3 f-w-600">{{ __('Login') }}</h2>
+</div>
+{{ Form::open(['route' => 'login', 'method' => 'post', 'id' => 'loginForm', 'class'=>'needs-validation','novalidate'   ]) }}
+@csrf
+@if (session('status'))
+<div class="mb-4 font-medium text-lg text-green-600 text-danger">
+    {{session('status') }}
+</div>
+@endif
+<div class="">
+    <div class="form-group mb-3">
+        <label for="email"
+            class="form-label d-flex align-items-center justify-content-between">{{ __('Email') }}</label>
+        <input class="form-control @error('email') is-invalid @enderror" id="email" type="email" name="email"
+            value="{{ old('email') }}" required autocomplete="email" autofocus>
+        @error('email')
+        <div class="invalid-feedback" role="alert">{{ $message }}</div>
+        @enderror
     </div>
-    {{ Form::open(['route' => 'login', 'method' => 'post', 'id' => 'loginForm', 'class'=>'needs-validation','novalidate'   ]) }}
-    @csrf
-    @if (session('status'))
-        <div class="mb-4 font-medium text-lg text-green-600 text-danger">
-            {{session('status') }}
-        </div>
+    <div class="form-group mb-3">
+        <label for="password"
+            class="form-label d-flex align-items-center justify-content-between">{{ __('Password') }}</label>
+        <input class="form-control @error('password') is-invalid @enderror" id="password" type="password"
+            name="password" required autocomplete="current-password">
+        @error('password')
+        <div class="invalid-feedback" role="alert">{{ $message }}</div>
+        @enderror
+
+    </div>
+
+    @if ($settings['recaptcha_module'] == 'yes')
+    @if (isset($settings['google_recaptcha_version']) && $settings['google_recaptcha_version'] == 'v2-checkbox')
+    <div class="form-group mb-4">
+        {!! NoCaptcha::display($settings['cust_darklayout'] == 'on' ? ['data-theme' => 'dark'] : []) !!}
+        @error('g-recaptcha-response')
+        <span class="small text-danger" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
+    @else
+    <div class="form-group mb-4">
+        <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response" class="form-control">
+        @error('g-recaptcha-response')
+        <span class="error small text-danger" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
     @endif
-    <div class="">
-        <div class="form-group mb-3">
-            <label for="email"
-                class="form-label d-flex align-items-center justify-content-between">{{ __('Email') }}</label>
-            <input class="form-control @error('email') is-invalid @enderror" id="email" type="email" name="email"
-                value="{{ old('email') }}" required autocomplete="email" autofocus>
-            @error('email')
-                <div class="invalid-feedback" role="alert">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="form-group mb-3">
-            <label for="password"
-                class="form-label d-flex align-items-center justify-content-between">{{ __('Password') }}</label>
-            <input class="form-control @error('password') is-invalid @enderror" id="password" type="password"
-                name="password" required autocomplete="current-password">
-            @error('password')
-                <div class="invalid-feedback" role="alert">{{ $message }}</div>
-            @enderror
-
-        </div>
-
-        @if ($settings['recaptcha_module'] == 'yes')
-            @if (isset($settings['google_recaptcha_version']) && $settings['google_recaptcha_version'] == 'v2-checkbox')
-                <div class="form-group mb-4">
-                    {!! NoCaptcha::display($settings['cust_darklayout'] == 'on' ? ['data-theme' => 'dark'] : []) !!}
-                    @error('g-recaptcha-response')
-                        <span class="small text-danger" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-            @else
-                <div class="form-group mb-4">
-                    <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response" class="form-control">
-                    @error('g-recaptcha-response')
-                        <span class="error small text-danger" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-            @endif
+    @endif
+    <div class="form-group mb-4">
+        @if (Route::has('password.request'))
+        <a href="{{ route('password.request', $lang) }}"
+            class="text-xs d-flex align-items-center justify-content-between">{{ __('Forgot Your Password?') }}</a>
         @endif
-        <div class="form-group mb-4">
-            @if (Route::has('password.request'))
-                <a href="{{ route('password.request', $lang) }}"
-                    class="text-xs d-flex align-items-center justify-content-between">{{ __('Forgot Your Password?') }}</a>
-            @endif
-        </div>
-        <div class="d-grid">
-            <button type="submit" class="btn btn-primary btn-block mt-2" id="login_button">{{ __('Login') }}</button>
-
-        </div>
-        @if ($settings['enable_signup'] == 'on')
-            <p class="my-4 text-center d-none">{{ __("Don't have an account?") }} <a href="{{ route('register',$lang) }}"
-                    class="text-primary">{{ __('Register') }}</a></p>
-        @endif
-
-        <div class="row d-none">   
-            <div class="col-6 d-grid">
-                <a href="{{ route('customer.login') }}"
-                    class="btn-login btn btn-secondary btn-block mt-2 text-white">{{ __('Customer Login') }}</a>
-
-            </div>
-            <div class="col-6 d-grid">
-                <a href="{{ route('vender.login') }}"
-                    class="btn-login btn btn-secondary btn-block mt-2 text-white">{{ __('Vendor Login') }}</a>
-            </div>
-        </div>
+    </div>
+    <div class="d-grid">
+        <button type="submit" class="btn btn-primary btn-block mt-2" id="login_button">{{ __('Login') }}</button>
 
     </div>
-    {{ Form::close() }}
+    @if ($settings['enable_signup'] == 'on')
+    <p class="my-4 text-center d-none">{{ __("Don't have an account?") }} <a href="{{ route('register',$lang) }}"
+            class="text-primary">{{ __('Register') }}</a></p>
+    @endif
+
+    <div class="row d-none">
+        <div class="col-6 d-grid">
+            <a href="{{ route('customer.login') }}"
+                class="btn-login btn btn-secondary btn-block mt-2 text-white">{{ __('Customer Login') }}</a>
+
+        </div>
+        <div class="col-6 d-grid">
+            <a href="{{ route('vender.login') }}"
+                class="btn-login btn btn-secondary btn-block mt-2 text-white">{{ __('Vendor Login') }}</a>
+        </div>
+    </div>
+
+</div>
+{{ Form::close() }}
 @endsection
 
 @push('custom-scripts')
-    <script>
-        $(document).ready(function() {
-            $("#loginForm").submit(function(e) {
-                $("#login_button").attr("disabled", true);
-                return true;
-            });
+<script>
+    $(document).ready(function() {
+        $("#loginForm").submit(function(e) {
+            $("#login_button").attr("disabled", true);
+            return true;
         });
-    </script>
-        {{-- @if ($settings['recaptcha_module'] == 'yes')
+    });
+</script>
+{{-- @if ($settings['recaptcha_module'] == 'yes')
         {!! NoCaptcha::renderJs() !!}
         @endif --}}
-    @if (isset($settings['recaptcha_module']) && $settings['recaptcha_module'] == 'yes')
-        @if (isset($settings['google_recaptcha_version']) && $settings['google_recaptcha_version'] == 'v2-checkbox')
-            {!! NoCaptcha::renderJs() !!}
-        @else
-            <script src="https://www.google.com/recaptcha/api.js?render={{ $settings['google_recaptcha_key'] }}"></script>
-            <script>
-                $(document).ready(function() {
-                    grecaptcha.ready(function() {
-                        grecaptcha.execute('{{ $settings['google_recaptcha_key'] }}', {
-                            action: 'submit'
-                        }).then(function(token) {
-                            $('#g-recaptcha-response').val(token);
-                        });
-                    });
-                });
-            </script>
-        @endif
-    @endif
+@if (isset($settings['recaptcha_module']) && $settings['recaptcha_module'] == 'yes')
+@if (isset($settings['google_recaptcha_version']) && $settings['google_recaptcha_version'] == 'v2-checkbox')
+{!! NoCaptcha::renderJs() !!}
+@else
+<script src="https://www.google.com/recaptcha/api.js?render={{ $settings['google_recaptcha_key'] }}"></script>
+<script>
+    $(document).ready(function() {
+        grecaptcha.ready(function() {
+            grecaptcha.execute('{{ $settings['
+                google_recaptcha_key '] }}', {
+                    action: 'submit'
+                }).then(function(token) {
+                $('#g-recaptcha-response').val(token);
+            });
+        });
+    });
+</script>
+@endif
+@endif
 @endpush
